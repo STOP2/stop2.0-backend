c8d1ebdff6afb132f0c31da8a036284dece192d7
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-09-12 20:40:48 +0300

    configured port to work on heroku

--- src/stop.py
+++ src/stop.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,7 +11,7 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=os.getenv('PORT', '5000'))
 
 class Foo:
 
