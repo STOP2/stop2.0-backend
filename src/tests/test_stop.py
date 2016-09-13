4796f522337c9b08947ebbe711640f12b27d592d
lapula <pulliainen.laur@gmail.com>
2016-09-13 16:04:41 +0300

    testit routelle

--- src/tests/test_stop.py
+++ src/tests/test_stop.py
@@ -1,10 +1,21 @@
 import unittest
+from src import stop
+from flask import json, jsonify
+
 
 class TestStopRoutes(unittest.TestCase):
+
+    def setUp(self):
+        stop.app.config['TESTING'] = True
+        self.app = stop.app.test_client()
 
     def test_hello(self):
         self.assertEqual('foo', 'foo')
 
+    def test_stops_get(self):
+        response = self.app.get('/stops?lat=1.0&lon=2.0')
+        data = json.loads(response.data)
+        self.assertEquals(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
