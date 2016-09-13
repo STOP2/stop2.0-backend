7ae1a1cc00a362832d9bff5117e6e97b3317764c
lapula <pulliainen.laur@gmail.com>
2016-09-13 16:03:46 +0300

    route

--- src/stop.py
+++ src/stop.py
@@ -1,6 +1,6 @@
-import os
-
 from flask import Flask
+from flask import request
+from flask import json
 
 app = Flask(__name__)
 
@@ -10,8 +10,20 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/stops', methods=['GET'])
+def get_stops():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+
+    # TODO use service to get json and return it
+    result = {}
+    result['lat'] = lat
+    result['lon'] = lon
+    return json.dumps(result)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT', '5000'))
+
 
 class Foo:
 
