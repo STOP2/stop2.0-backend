ef9b1ffcc97ccd298ceeaac8559e8aa8a4d854a6
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-09-13 16:18:39 +0300

    moved services to separate class, implemented get_busses_by_stop_id

---
+++ src/services.py
@@ -0,0 +1,44 @@
+import datetime
+import requests
+import json
+
+class DigitransitAPIService:
+    def __init__(self):
+        self.url = 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
+        #datetime.datetime.now().strftime("%Y%m%d")
+
+    def get_stops(self):
+        return ''
+
+    def get_stops_near_coordinates(self):
+        return ''
+
+    def get_busses_by_stop_id(self, stop_id):
+        query =  ("{stop(id: \"%s\") {"
+                      "  name"
+                      "  code"
+                      "  stoptimesForServiceDate(date: \"%s\"){"
+                      "     pattern {"
+                      "         id"
+                      "         name"
+                      "         directionId"
+                      "         route {"
+                      "             gtfsId"
+                      "             shortName"
+                      "         }"
+                      "     }"
+                      "     stoptimes {"
+                      "         serviceDay"
+                      "      	scheduledArrival"
+                      "    	    realtimeArrival"
+                      "      }"
+                      "    }"
+                      "  }"
+                      "}") % (stop_id, datetime.datetime.now().strftime("%Y%m%d"))
+
+        return self.getQuery(query)
+
+    def getQuery(self, query):
+        headers = {'Content-Type': 'application/graphql'}
+        response = requests.post(self.url, data=query, headers=headers)
+        return json.dumps(response.text)
--- src/stop.py
+++ src/stop.py
@@ -1,34 +1,28 @@
 import os
-import datetime
 
 from flask import Flask
 
+from src import services
+
 app = Flask(__name__)
 
+digitransitAPIService = services.DigitransitAPIService()
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/test')
+def digitransitTest():
+    return digitransitAPIService.get_busses_by_stop_id("HSL:1362141")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT', '5000'))
+
 
 class Foo:
 
     def hello(self):
         return 'foo'
 
-class DigitransitAPIService:
-    def __init__(self):
-        self.url = 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
-        #datetime.datetime.now().strftime("%Y%m%d")
-
-    def get_stops(self):
-        return ''
-
-    def _get_stops_near_coordinates(self):
-        return ''
-
-    def _get_busses_by_stop_id(self):
-        return ''
