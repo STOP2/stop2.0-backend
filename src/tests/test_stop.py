0a67faa55403630dc48000a6efcf57e4684e57e4
lapula <pulliainen.laur@gmail.com>
2016-09-14 14:19:56 +0300

    uusi import yritys

--- src/tests/test_stop.py
+++ src/tests/test_stop.py
@@ -1,20 +1,18 @@
 import unittest
-import stop
-from flask import json, jsonify
+from src.stop import app
 
 
 class TestStopRoutes(unittest.TestCase):
 
     def setUp(self):
-        stop.app.config['TESTING'] = True
-        self.app = stop.app.test_client()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
 
     def test_hello(self):
         self.assertEqual('foo', 'foo')
 
     def test_stops_get(self):
         response = self.app.get('/stops?lat=1.0&lon=2.0')
-        data = json.loads(response.data)
         self.assertEquals(response.status_code, 200)
 
 if __name__ == '__main__':
