98f6d3ff2692284766e6b22f5eaca2a350533df6
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-09-19 12:00:55 +0300

    get_busses_by_stop_id done

--- src/services.py
+++ src/services.py
@@ -1,6 +1,8 @@
 import datetime
 import requests
 import json
+import math
+import re
 
 class DigitransitAPIService:
     def __init__(self):
@@ -26,19 +28,44 @@ class DigitransitAPIService:
                       "         directionId"
                       "         route {"
                       "             gtfsId"
+                      "             longName"
                       "             shortName"
                       "         }"
                       "     }"
                       "     stoptimes {"
                       "         serviceDay"
-                      "      	scheduledArrival"
                       "    	    realtimeArrival"
                       "      }"
                       "    }"
                       "  }"
                       "}") % (stop_id, datetime.datetime.now().strftime("%Y%m%d"))
 
-        return self.getQuery(url, query, headers)
+        data = json.loads(self.getQuery(url, query, headers))["data"]["stop"]
+
+        lines = data["stoptimesForServiceDate"]
+
+        current_time = datetime.datetime.now()
+
+        stop = {'stop_name': data["name"], 'stop_code': data["code"], 'schedule': []}
+        schedule = []
+        for line in lines:
+            destination = line["pattern"]["route"]["longName"]
+
+            stoptimes = line["stoptimes"]
+            for time in stoptimes:
+                arrival_time = datetime.datetime.fromtimestamp(time["serviceDay"] + time["realtimeArrival"])
+                arrival = math.floor((arrival_time - current_time).total_seconds() / 60.0)
+                if current_time < arrival_time:
+                    schedule.append({'line': line["pattern"]["route"]["shortName"],
+                                     'destination': destination,
+                                     'arrival': arrival,
+                                     'routeId': line["pattern"]["route"]["gtfsId"],
+                                     'direction': line["pattern"]["directionId"]})
+
+        sorted_list = sorted(schedule, key=lambda k: k['arrival'])
+        stop["schedule"] = sorted_list
+
+        return stop
 
     def getQuery(self, url, query, headers):
         response = requests.post(url, data=query, headers=headers)
