63c1f409630916550ef584142b34676435e83057
aorin <aorin@users.noreply.github.com>
2016-09-19 12:32:25 +0300

    get_stops updated

--- src/services.py
+++ src/services.py
@@ -7,10 +7,20 @@ import math
 class DigitransitAPIService:
     def __init__(self):
         self.url = 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
-        #datetime.datetime.now().strftime("%Y%m%d")
+        self.headers = {'Content-Type': 'application/graphql'}
 
+    def get_stops(self, lat, lon):
+        data = {}
+        stops = []
+        stop_ids = self.get_stops_near_coordinates(lat, lon)
 
-    def get_stops_near_cordinates(self, lat, lon, radius=20):
+        for stop_id in stop_ids:
+            stops.append({"stop": self.get_busses_by_stop_id(stop_id)})
+
+        data["stops"] = stops
+        return data
+
+    def get_stops_near_coordinates(self, lat, lon, radius=160):
         query = ("{stopsByRadius(lat:%f, lon:%f, radius:%d) {"
                  "  edges {"
                  "      node {"
@@ -30,24 +40,9 @@ class DigitransitAPIService:
             stoplist.append(n['node']['stop']['gtfsId'])
         return stoplist
 
-    def get_stops(self, x, y):
-        data = {}
-        stopsData = []
-        #stops = self.get_stops_near_coordinates(x, y)
-
-        #for stop in stops:
-        #    stopsData.append(self.get_busses_by_stop_id(stop.id)["data"])
-
-        stop = json.loads(self.get_busses_by_stop_id("HSL:1362141"))
-        stopsData.append(stop["data"])
-        data["stops"] = stopsData
-
-        return data
-
-
     def get_busses_by_stop_id(self, stop_id):
-        url = 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
-        headers = {'Content-Type': 'application/graphql'}
+        #url = 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
+        #headers = {'Content-Type': 'application/graphql'}
         query =  ("{stop(id: \"%s\") {"
                       "  name"
                       "  code"
@@ -70,7 +65,7 @@ class DigitransitAPIService:
                       "  }"
                       "}") % (stop_id, datetime.datetime.now().strftime("%Y%m%d"))
 
-        data = json.loads(self.getQuery(url, query, headers))["data"]["stop"]
+        data = json.loads(self.getQuery(query))["data"]["stop"]
 
         lines = data["stoptimesForServiceDate"]
 
@@ -96,7 +91,7 @@ class DigitransitAPIService:
 
         return stop
 
-    def getQuery(self, url, query, headers):
-        response = requests.post(url, data=query, headers=headers)
+    def getQuery(self, query):
+        response = requests.post(self.url, data=query, headers=self.headers)
 
         return response.text
