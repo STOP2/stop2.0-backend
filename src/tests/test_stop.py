42e9188287837434167cebf13c78c7a2114fee1e
Henna Ruohonen <henna.ruohonen@helsinki.fi>
2016-09-19 12:14:01 +0300

    import testing

--- src/tests/test_stop.py
+++ src/tests/test_stop.py
@@ -1,20 +1,23 @@
 import unittest
 import sys
-#from src.stop import app
+# sys.path.append('../src')
+import stop
 
 
 class TestStopRoutes(unittest.TestCase):
 
-  #  def setUp(self):
-#      app.config['TESTING'] = True
-  #      self.app = app.test_client()
+    def setUp(self):
+        app = stop.app
+        print('foo')
+        app.config['TESTING'] = True
+        self.app = app.test_client()
 
     def test_hello(self):
         self.assertEqual('foo', 'foo')
 
-    # def test_stops_get(self):
-    #     response = self.app.get('/stops?lat=1.0&lon=2.0')
-    #     self.assertEquals(response.status_code, 200)
+    def test_stops_get(self):
+        response = self.app.get('/stops?lat=1.0&lon=2.0')
+        self.assertEquals(response.status_code, 200)
 
 if __name__ == '__main__':
     print(sys.path)
