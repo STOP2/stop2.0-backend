1a5335d682adbc17e707a1be0f3acab229a63a90
aorin <aorin@users.noreply.github.com>
2016-09-19 13:16:52 +0300

    test setUp added

--- src/tests/test_services.py
+++ src/tests/test_services.py
@@ -7,8 +7,14 @@ class TestDigitransitAPIService(unittest.TestCase):
         digitransitAPIService = services.DigitransitAPIService()
 
     def test_get_stops(self):
-
         self.assertEqual('foo', 'foo')
+
+    def test_get_stops_near_coordinates(self):
+        self.assertEqual('foo', 'foo')
+
+    def test_get_busses_by_stop_id(self):
+        return
+
 
 
 if __name__ == '__main__':
