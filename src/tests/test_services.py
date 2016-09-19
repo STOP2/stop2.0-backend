acb98e4ecbf548f47e3b35a395b05bfc17060ff9
aorin <aorin@users.noreply.github.com>
2016-09-19 12:41:09 +0300

    test_services file added

---
+++ src/tests/test_services.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestStopRoutes(unittest.TestCase):
+
+    def test_get_stops(self):
+        self.assertEqual('foo', 'foo')
+
+
+if __name__ == '__main__':
+    unittest.main()
