81b765aa82a8b35c5c04261afa18050c415186e2
lapula <pulliainen.laur@gmail.com>
2016-09-20 15:13:24 +0300

    runko_routelle

--- src/stop.py
+++ src/stop.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask
 from flask import request
 from flask import json
+import paho.mqtt.publish as publish
 
 import services
 
@@ -21,13 +22,21 @@ def hello_world():
 def digitransit_test():
     return json.dumps(digitransitAPIService.get_stops(60.203978, 24.9633573))
 
+@app.route('/stoprequests', methods=['POST'])
+def stoprequest():
+    testData = '{"bus_id": "1234", "stop_id": "56789", "request_type": "stop"}'
+    data = json.loads(testData)
+    bus_id = data["bus_id"]
+    del data["bus_id"]
+    data = json.dumps(data)
+    publish.single(topic="stoprequests/"+bus_id, payload=data, hostname="epsilon.fixme.fi")
+    return ""
 
 @app.route('/stops', methods=['GET'])
 def stops():
     lat = float(request.args.get('lat'))
     lon = float(request.args.get('lon'))
     result = digitransitAPIService.get_stops(lat, lon)
-    print(result)
     return json.dumps(result)
 
 if __name__ == '__main__':
