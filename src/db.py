5c3836bcedfc521f6bc6034f8e4961518bb194b0
jaahon <jan.a.ahonen@helsinki.fi>
2016-09-20 16:44:29 +0300

    Add a database class that makes a connection pool to heroku database on initialization

---
+++ src/db.py
@@ -0,0 +1,20 @@
+import psycopg2
+import psycopg2.pool
+
+
+class Database:
+    def __init__(self):
+        self.pool = psycopg2.pool.ThreadedConnectionPool(1, 5, host='ec2-54-217-213-156.eu-west-1.compute.amazonaws.com', user='hqcqeccowirrxj', database='d8r79ti4iuqq79', password='Cf5uDO5AbUeExQz5a6KWSjnKDF')
+
+    def get_connection(self):
+        return self.pool.getconn()
+    
+    def put_connection(self, conn):
+        self.pool.putconn(conn)
+    
+    def default(self):
+        conn = self.get_connection()
+        # conn.execute("SQL")
+        conn.commit()
+        self.put_connection(conn)
+
