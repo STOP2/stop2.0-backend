6e8421a00e28a88a5e98fe235ceedac4d79c0edf
jaahon <jan.a.ahonen@helsinki.fi>
2016-09-26 13:21:50 +0300

    Use environment variables to set database related values

--- src/db.py
+++ src/db.py
@@ -1,9 +1,14 @@
 import psycopg2
 import psycopg2.pool
+import os
 
 
 class Database:
     def __init__(self):
+        host = os.environ['DBHOST'] or 'localhost:5432'
+        user = os.environ['DBUSER'] or 'stop'
+        password = os.environ['DBPASS'] or 'stop'
+        db = os.environ['DB'] or 'stop'
         self.pool = psycopg2.pool.ThreadedConnectionPool(1, 5, host='ec2-54-217-213-156.eu-west-1.compute.amazonaws.com', user='hqcqeccowirrxj', database='d8r79ti4iuqq79', password='Cf5uDO5AbUeExQz5a6KWSjnKDF')
 
     def get_connection(self):
