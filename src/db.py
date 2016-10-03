d045fc1ad523ac0c902e20759b245f3b31eb3158
jaahon <jan.a.ahonen@helsinki.fi>
2016-10-03 11:47:08 +0300

    Fix setting database port in travis

--- src/db.py
+++ src/db.py
@@ -5,7 +5,7 @@ import os
 
 class Database:
     def __init__(self):
-        self.pool = psycopg2.pool.ThreadedConnectionPool(1, 5, host=os.getenv('DBHOST', 'ec2-54-217-213-156.eu-west-1.compute.amazonaws.com'), user=os.getenv('DBUSER','hqcqeccowirrxj'), database=os.getenv('DBNAME','d8r79ti4iuqq79'), password=os.getenv('DBPASS','Cf5uDO5AbUeExQz5a6KWSjnKDF'))
+        self.pool = psycopg2.pool.ThreadedConnectionPool(1, 5, host=os.getenv('DBHOST', 'ec2-54-217-213-156.eu-west-1.compute.amazonaws.com'), port=os.getenv('DBPORT', ''), user=os.getenv('DBUSER','hqcqeccowirrxj'), database=os.getenv('DBNAME','d8r79ti4iuqq79'), password=os.getenv('DBPASS','Cf5uDO5AbUeExQz5a6KWSjnKDF'))
 
     def get_connection(self):
         return self.pool.getconn()
