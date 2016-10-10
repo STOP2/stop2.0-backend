4298ee56c068964a1791edd0602e3ab938079f2d
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-10-10 11:35:57 +0300

    changed vehicle_id to trip_id

--- src/services.py
+++ src/services.py
@@ -89,13 +89,12 @@ class DigitransitAPIService:
                 arrival_time = datetime.datetime.fromtimestamp(time["serviceDay"] + time["realtimeArrival"])
                 arrival = math.floor((arrival_time - current_time).total_seconds() / 60.0)  # Arrival in minutes
                 if current_time < arrival_time:
-                    schedule.append({'vehicle_id': line["pattern"]["code"],
+                    schedule.append({'vehicle_id': time["trip"]["gtfsId"][4:], #Technically trip_id
                                      'line': line["pattern"]["route"]["shortName"],
                                      'destination': destination,
                                      'arrival': arrival,
                                      'routeId': line["pattern"]["route"]["gtfsId"],
-                                     'vehicle_type': data["vehicleType"],
-                                     'trip_id': time["trip"]["gtfsId"][4:]
+                                     'vehicle_type': data["vehicleType"]
                                      })
 
         sorted_list = sorted(schedule, key=lambda k: k['arrival'])
