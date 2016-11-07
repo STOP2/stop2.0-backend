8bb6b3b7b981c9b385ee2b9ab2b3c7dcab3edcfb
heileino <heileino@cs.helsinki.fi>
2016-11-07 13:37:05 +0200

    refactoring

--- src/push_notifications.py
+++ src/push_notifications.py
@@ -1,12 +1,16 @@
 from pyfcm import FCMNotification
-import db
 
-push_service = FCMNotification(api_key='AIzaSyDzuwUdIs5sog6UAv1dTzx2JJuCG2yOkcA')
 
-database = db.Database()
-devices = database.get_device_ids()
-#lisättävä ehto, joka rajaa notifikaation saajat
-registration_ids = [devices]
-message_title = "Bussi saapuu!"
-message_body = "Tilaamasi bussi saapuu pysäkillesi hetken kuluttua"
-result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
+class PushNotifications():
+
+    def __init__(self, db):
+        self.db = db
+
+    def get_push_notification(self):
+        push_service = FCMNotification(api_key='AIzaSyDzuwUdIs5sog6UAv1dTzx2JJuCG2yOkcA')
+        devices = self.db.get_device_ids()
+        #lisättävä ehto, joka rajaa notifikaation saajat
+        registration_ids = [devices]
+        message_title = "Bussi saapuu!"
+        message_body = "Tilaamasi bussi saapuu pysäkillesi hetken kuluttua"
+        result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
