daa6145318ab93b0d83c2081ccb088bed0b82c27
heileino <heileino@cs.helsinki.fi>
2016-11-07 12:36:38 +0200

    added device list to push_notifications.py

--- src/push_notifications.py
+++ src/push_notifications.py
@@ -1,9 +1,11 @@
 from pyfcm import FCMNotification
+import db
 
 push_service = FCMNotification(api_key='AIzaSyD4bExMYdOyBpFOFm-S4JqJsb4vukUy1eM')
 
-
-registration_ids = ["<device registration_id 1>", "<device registration_id 2>", ...]
+database = db.Database()
+devices = database.get_device_ids()
+registration_ids = [devices]
 message_title = "Bussi saapuu!"
 message_body = "Tilaamasi bussi saapuu pysäkillesi hetken kuluttua"
 result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
