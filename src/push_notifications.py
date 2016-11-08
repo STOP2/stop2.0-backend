17274ca9fa5ff085a3aaf72fa7fcad54942d8aa2
heileino <heileino@cs.helsinki.fi>
2016-11-08 12:37:29 +0200

    now sending push notification to hard coded device

--- src/push_notifications.py
+++ src/push_notifications.py
@@ -7,10 +7,12 @@ class PushNotifications():
         self.db = db
 
     def send_push_notification(self, trip_id, stop_id):
-        push_service = FCMNotification(api_key='AIzaSyDzuwUdIs5sog6UAv1dTzx2JJuCG2yOkcA')
-        devices = self.db.get_device_ids(trip_id, stop_id)
-        registration_ids = [devices]
-        message_title = "Bussi saapuu!"
-        message_body = "Tilaamasi bussi saapuu pysäkillesi hetken kuluttua"
-        result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
-        print(result)
+        push_service = FCMNotification(api_key='AIzaSyBbs3cP4o7hJGwbJ51Rhf6zBdAsqnw2-nM')
+        #TYÖN ALLA, toistaiseksi käytetään kovakoodattua registration_id:tä. #devices = self.db.get_device_ids(trip_id, stop_id)
+        registration_ids = ['e60w7vyvSrY:APA91bGMqp8VNOinW4fD61CbS9_HI6Ty1aZc98jl7fZhoJWh3JGnIAn7IimN7fUB8r9bKhB8V7vua-pArr8CsChxNDFC46lhCsj8fHoIsMTm2CFDarFxeGrFA69o-JfnJIg4WVDPYr4b']#json.dumps([devices])
+        data_message = {
+            "title": "Kulkuneuvo saapuu!",
+            "message": "Tilaamasi kulkuneuvo on pysäkilläsi hetken kuluttua"
+        }
+        result = push_service.notify_multiple_devices(registration_ids=registration_ids,
+                                                      data_message=data_message)
