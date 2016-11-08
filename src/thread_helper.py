1d408fe8a24a5a79fa7648466955f271b5e73bb5
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-11-08 11:21:05 +0200

    finished background task for fetching all realtime data

--- src/thread_helper.py
+++ src/thread_helper.py
@@ -1,12 +1,20 @@
 import threading
+import os
 
-
-# Calls worker_func every interval seconds for interations times (0 -> eternal loop)
-def do_every(interval, worker_func, iterations=0):
+# Calls worker_func every interval seconds for interations times (0 is eternal loop)
+def do_every(lockname, interval, worker_func, iterations=0):
     if iterations != 1:
         threading.Timer(
             interval,
-            do_every, [interval, worker_func, 0 if iterations == 0 else iterations - 1]
+            do_every, [lockname, interval, worker_func, 0 if iterations == 0 else iterations - 1]
         ).start()
+    elif iterations == 1:
+        os.environ[lockname] = 'FALSE'
 
     worker_func()
+
+#Start running iterations only if environment value [lockname] is 'FALSE' or doesn't exist
+def start_do_every(lockname, interval, worker_func, iterations=0):
+    if os.getenv(lockname, 'FALSE') == 'FALSE':
+        os.environ[lockname] = 'TRUE'
+        do_every(lockname, interval, worker_func, iterations)
