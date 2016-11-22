5ea1e238710c98385e3ac18e358e28c26853f9d7
jaahon <jan.a.ahonen@helsinki.fi>
2016-11-22 10:23:27 +0200

    Give FCM api key from environment variable

--- src/push_notification_service.py
+++ src/push_notification_service.py
@@ -1,10 +1,11 @@
 from pyfcm import FCMNotification
+import os
 
 
 class PushNotificationService():
 
     def send_push_notification(self, registration_ids):
-        push_service = FCMNotification(api_key='AIzaSyBbs3cP4o7hJGwbJ51Rhf6zBdAsqnw2-nM')
+        push_service = FCMNotification(api_key=os.getenv('FCM_API_KEY'))
         #registration_ids = ['e60w7vyvSrY:APA91bGMqp8VNOinW4fD61CbS9_HI6Ty1aZc98jl7fZhoJWh3JGnIAn7IimN7fUB8r9bKhB8V7vua-pArr8CsChxNDFC46lhCsj8fHoIsMTm2CFDarFxeGrFA69o-JfnJIg4WVDPYr4b'] #([devices])
         data_message = {
             "title": "Kulkuneuvo saapuu!",
