fbeb210c89d4eba311601e300c5b61fd8023455e
heileino <heileino@cs.helsinki.fi>
2016-11-29 16:19:07 +0200

    removed unnecessary comments

--- src/push_notification_service.py
+++ src/push_notification_service.py
@@ -6,7 +6,6 @@ class PushNotificationService():
 
     def send_push_notifications(self, registration_ids):
         push_service = FCMNotification(api_key=os.getenv('FCM_API_KEY'))
-        #registration_ids = ['e60w7vyvSrY:APA91bGMqp8VNOinW4fD61CbS9_HI6Ty1aZc98jl7fZhoJWh3JGnIAn7IimN7fUB8r9bKhB8V7vua-pArr8CsChxNDFC46lhCsj8fHoIsMTm2CFDarFxeGrFA69o-JfnJIg4WVDPYr4b'] #([devices])
         data_message = {
             "title": "Kulkuneuvo saapuu!",
             "message": "Tilaamasi kulkuneuvo on pysäkilläsi hetken kuluttua"
