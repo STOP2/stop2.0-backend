5186c4151df0494b331bdd47191a9614f56b3085
heileino <heileino@cs.helsinki.fi>
2016-12-07 00:34:00 +0200

    pep8 refactoring

--- src/tests/test_stop.py
+++ src/tests/test_stop.py
@@ -8,7 +8,6 @@ class TestStopRoutes(unittest.TestCase):
         stop.app.config['TESTING'] = True
         self.app = stop.app.test_client()
 
-
     def test_stops_get(self):
         response = self.app.get('/stops?lat=1.0&lon=2.0')
         self.assertEqual(response.status_code, 200)
@@ -18,9 +17,9 @@ class TestStopRoutes(unittest.TestCase):
         self.assertTrue(response.data.find(b"L\\u00e4nsiterminaali") != -1)
 
     def test_stopsrequests_post(self):
-        jsonString = '{"trip_id": "1234", "stop_id": "HSL:1282106", "device_id": "123", "push_notification": false}'
-        response = self.app.post('/stoprequests', data=jsonString, content_type='application/json')
-        self.assertEquals(response.status_code, 200)
+        json_string = '{"trip_id": "1234", "stop_id": "HSL:1282106", "device_id": "123", "push_notification": false}'
+        response = self.app.post('/stoprequests', data=json_string, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
