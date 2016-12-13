81c853676fc23558a5c2c0d8dc2ef5d9517c87f9
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-12-13 10:37:22 +0200

    added pydocs to thread_helper.py

--- src/thread_helper.py
+++ src/thread_helper.py
@@ -1,8 +1,17 @@
 import threading
 import os
 
-# Calls worker_func every interval seconds for interations times (0 is eternal loop)
+
 def do_every(lockname, interval, worker_func, iterations=0):
+    """
+    Calls worker_func every interval seconds for interations times (0 is eternal loop) or until envinroment variable
+    named after lockname is STOP.
+
+    :param lockname: name of envinroment variable which specifies if certain worker is running and when to stop
+    :param interval: interval the worker is run in seconds
+    :param worker_func: worker function
+    :param iterations: number of iterations, 0 means infinite
+    """
     if os.environ[lockname] == 'STOP':
         os.environ[lockname] = 'FALSE'
         return
@@ -16,8 +25,17 @@ def do_every(lockname, interval, worker_func, iterations=0):
 
     worker_func()
 
-#Start running iterations only if environment value [lockname] is 'FALSE' or doesn't exist
+
 def start_do_every(lockname, interval, worker_func, iterations=0):
+    """
+    Starts function do_every with parameters given to it unless environment variable specified by lockname is already
+    TRUE, in which case it's already running.
+
+    :param lockname: name of envinroment variable which specifies if certain worker is running and when to stop
+    :param interval: interval the worker is run in seconds
+    :param worker_func: worker function
+    :param iterations: number of iterations, 0 means infinite
+    """
     if os.getenv(lockname, 'FALSE') == 'FALSE':
         os.environ[lockname] = 'TRUE'
         do_every(lockname, interval, worker_func, iterations)
